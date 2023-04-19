01d878e9a52d991986578d617134063768668f7c
Brian Padilla <72839495+brianpadilla3072@users.noreply.github.com>
2023-04-19 19:31:06 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -3,10 +3,10 @@ import {config} from "dotenv"
 config();
 
 export default {
-     host: process.env.HOST || "localhost" ||  "containers-us-west-196.railway.app" ,
-     database: process.env.DATABASE || "rest-api-node-js"|| "railway",
-     user: process.env.USER || "root"|| "root",
-     password: process.env.PASSWORD || "1234" || "LyeVEnUFwfJ93IkpRcoE",
+     host: process.env.HOST || "localhost",
+     database: process.env.DATABASE || "rest-api-node-js",
+     user: process.env.USER || "root",
+     password: process.env.PASSWORD || "1234" ,
      secretPublic: process.env.SECRETPUBLIC || "N0RMA",
-     port: process.env.PORT || "3000" || "6891"
+     port: process.env.PORT || "3000"
 }
